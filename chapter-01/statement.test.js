c9585081b203fcd109655c5bde6c987d98bbbdc6
andrea <kjkandrea@gmail.com>
2021-10-29 20:22:25 +0900

    Add babel env

--- chapter-01/statement.test.js
+++ chapter-01/statement.test.js
@@ -1,3 +1,8 @@
+import invoices from './data/invoices.json'
+
+
 describe('나도 테스트 짜볼래', () => {
   test('크아아아', () => expect([1, 2].reduce((a, b) => a + b)).toBe(3))
+
+  test('import도 할거다!!!', () => expect(invoices).toBeTruthy())
 })
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  moduleFileExtensions: ["js", "json", "jsx"],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest'
+  },
   testEnvironment: 'node',
   testMatch: ['**/**/?(*.)(test).js'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/']
 }

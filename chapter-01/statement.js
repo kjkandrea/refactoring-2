ca468da81bcdb35ce231fb9e1f6224ca86f303dc
andrea <kjkandrea@gmail.com>
2021-10-30 16:31:21 +0900

    Refactor : statementData

--- chapter-01/statement.js
+++ chapter-01/statement.js
@@ -1,11 +1,14 @@
 function statement(invoice, plays) {
-  return renderPlainText(invoice, plays)
+  const statementData = {}
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays)
 }
 
-function renderPlainText(invoice, plays) {
-  let result = `청구내역 (고객명: ${invoice.customer})\n`;
+function renderPlainText(data, plays) {
+  let result = `청구내역 (고객명: ${data.customer})\n`;
 
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     // 청구내역을 출력한다.
     result += `${playFor(perf).name} : ${usd(amountFor(perf) )} (${
       perf.audience
@@ -68,7 +71,7 @@ function renderPlainText(invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf)
     }
     return result;
@@ -76,7 +79,7 @@ function renderPlainText(invoice, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;

ed520ee94b0a10e52c54568cd95b9a8e53580f40
andrea <kjkandrea@gmail.com>
2021-10-30 15:47:08 +0900

    Refactor format

--- chapter-01/statement.js
+++ chapter-01/statement.js
@@ -5,12 +5,6 @@ function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
-
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf)
 
@@ -64,6 +58,14 @@ function statement(invoice, plays) {
       result += Math.floor(aPerformance.audience / 5);
     }
     return result;
+  }
+
+  function format(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber);
   }
 }
 

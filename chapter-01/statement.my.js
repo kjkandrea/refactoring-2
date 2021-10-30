f016c3246602991e1c2d7aae65a8cf9e9876545b
andrea <kjkandrea@gmail.com>
2021-10-30 13:44:55 +0900

    Update SOF : getDefaultAmountOfGenre
    명세는 제공되지만 뭔가 더 복잡해진 기분..?

--- chapter-01/statement.my.js
+++ chapter-01/statement.my.js
@@ -1,63 +1,82 @@
-function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `청구내역 (고객명: ${invoice.customer})\n`;
+const rateTable = {
+  tragedy: {
+    amount: 40000,
+    extraAmountAudienceCountLimit: 30,
+    extraAmountOfAudienceOverCount: 1000,
+    extraAmountOfAudienceCount: 0,
+    extraAmount: 0,
+  },
+  comedy: {
+    amount: 30000,
+    extraAmountAudienceCountLimit: 20,
+    extraAmountOfAudienceOverCount: 500,
+    extraAmountOfAudienceCount: 300,
+    extraAmount: 10000,
+  },
+}
+
+function getDefaultAmountOfGenre (rateTable, genre, audienceCount) {
+  const { amount } = rateTable[genre]
+  if (amount === undefined) {
+    throw new Error(`알 수 없는 장르 : ${genre}`)
+  }
+  return amount + getExtraAmountOfAudienceCount(rateTable, genre, audienceCount)
+}
+
+function getExtraAmountOfAudienceCount (rateTable, genre, audienceCount) {
+  const {
+    extraAmountAudienceCountLimit,
+    extraAmountOfAudienceOverCount,
+    extraAmountOfAudienceCount,
+    extraAmount,
+  } = rateTable[genre]
+  if (extraAmountAudienceCountLimit >= audienceCount) {
+    return 0
+  }
+  const audienceOverCountAmount = extraAmountOfAudienceOverCount *
+    (audienceCount - extraAmountAudienceCountLimit)
+  const audienceCountAmount = extraAmountOfAudienceCount * audienceCount
+  return extraAmount + audienceOverCountAmount + audienceCountAmount
+}
+
+function statement (invoice, plays) {
+  let totalAmount = 0
+  let volumeCredits = 0
+  let result = `청구내역 (고객명: ${invoice.customer})\n`
 
   const format = getUSDFormat()
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = plays[perf.playID]
 
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case 'tragedy': // 비극
-        thisAmount = 40000;
-
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        thisAmount = 30000;
-
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르 : ${play.type}`);
-    }
+    let thisAmount = getDefaultAmountOfGenre(rateTable, play.type, perf.audience)
 
     // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(perf.audience - 30, 0)
 
     // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ('comedy' === play.type) {
-      volumeCredits += Math.floor(perf.audience / 5);
+      volumeCredits += Math.floor(perf.audience / 5)
     }
 
     result += `${play.name} : ${format(thisAmount / 100)} (${
       perf.audience
-    }석)\n`;
-    totalAmount += thisAmount;
+    }석)\n`
+    totalAmount += thisAmount
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `총액: ${format(totalAmount / 100)}\n`
+  result += `적립 포인트: ${volumeCredits}점\n`
 
-  return result;
+  return result
 }
 
-function getUSDFormat() {
+function getUSDFormat () {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2,
-  }).format;
+  }).format
 }
 
-export default statement;
+export default statement

ff802c6180f0f774435c6ef5a2ba0a1a9e658be2
andrea <kjkandrea@gmail.com>
2021-10-30 16:53:25 +0900

    Refactor : volumeCreditsFor

--- chapter-01/statement.js
+++ chapter-01/statement.js
@@ -8,6 +8,7 @@ function statement(invoice, plays) {
     const result = {...aPerformance}
     result.play = playFor(result)
     result.amount = amountFor(result)
+    result.volumeCredits = volumeCreditsFor(result)
     return result;
   }
 
@@ -42,6 +43,15 @@ function statement(invoice, plays) {
 
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ('comedy' === aPerformance.play.type) {
+      result += Math.floor(aPerformance.audience / 5);
+    }
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -59,15 +69,6 @@ function renderPlainText(data, plays) {
 
   return result;
 
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-    return result;
-  }
-
   function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
@@ -79,7 +80,7 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf)
+      result += perf.volumeCredits;
     }
     return result;
   }

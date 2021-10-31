fa501cdab577e417b7429b6875d9d7f1f38691b1
andrea <kjkandrea@gmail.com>
2021-10-31 15:44:13 +0900

    Refactor : create TragedyCalculator, ComedyCalculator
    조건부 로직을 다형성으로 바꾸기

--- chapter-01/createStatementData.js
+++ chapter-01/createStatementData.js
@@ -32,7 +32,12 @@ export default function createStatementData(invoice, plays) {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay)
+  // return new PerformanceCalculator(aPerformance, aPlay)
+  switch(aPlay.type) {
+    case 'tragedy': return new TragedyCalculator(aPerformance, aPlay)
+    case 'comedy': return new ComedyCalculator(aPerformance, aPlay)
+    default: throw new Error(`알 수 없는 장르 : ${this.play.type}`);
+  }
 }
 
 class PerformanceCalculator {
@@ -75,6 +80,31 @@ class PerformanceCalculator {
     if ('comedy' === this.play.type) {
       result += Math.floor(this.performance.audience / 5);
     }
+    return result;
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+
     return result;
   }
 }
